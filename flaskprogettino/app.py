d99edaaf91e18b69b1895f3bda06787e3dfed741
turatistefanogalvani 123 <turati.stefano@iisgalvanimi.edu.it>
2025-04-04 08:56:47 +0000

    aggiunta lista

--- flaskprogettino/app.py
+++ flaskprogettino/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+lista = ["ciao1","ciao2","ciao3","ciao4","ciao5"]
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", x = lista)
+
 if __name__ == '__main__':
     app.run(debug=True)
