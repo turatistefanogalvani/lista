f83b355ff677fa2ee7011c1af1279f2500c9fd93
turatistefanogalvani 123 <turati.stefano@iisgalvanimi.edu.it>
2025-04-04 10:59:34 +0000

    a

--- flaskprogettino/app.py
+++ flaskprogettino/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-lista = ["ciao1","ciao2","ciao3","ciao4","ciao5"]
+lista = []
 @app.route('/')
 def home():
     return render_template("index.html", x = lista)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+#ottiene elemento dal form
+    elemento = request.form['elemento']
+#aggiunge alla lista
+    if elemento:
+        lista.append(elemento)
+        return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista):
+        lista.pop(indice)
+    return redirect(url_for('home'))
+
+@app.route('/pulisci', methods=['POST'])
+def pulisci():
+    lista.clear()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
